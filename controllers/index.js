8df4ed2d6882f476199b24df32444b8741fa2e64
David Wu <dwu0508@gmail.com>
2022-03-28 23:16:55 -0500

    added controllers

--- controllers/index.js
+++ controllers/index.js
@@ -70,11 +70,51 @@ const createChore = async (req, res) => {
     }
 }
 
-const findAllChores = async (req, res) => {
+const getAllChores = async (req, res) => {
     try {
         const chores = await Chore.find()
         return res.status(200).json({ chores })
     } catch (err) {
+        return res.status(500).send(err.message)
+    }
+}
+
+const getChoreById = async (req, res) => {
+    try {
+        const { id } = req.params
+        const chore = await Chore.findById(id)
+        if (chore) {
+            return res.status(200).json({ chore })
+        }
+        return res.status(400).send('Chore with the specified id does not exist.')
+    } catch (err) {
+        return res.status(500).send(err.message)
+    }
+} 
+
+const updateChore = async (req, res) => {
+    try {
+        const { id } = req.params
+        const chore = await Chore.findByIdAndUpdate(id, req.body, { new: true})
+            if (!chore) {
+                res.status(500).send('Chore not found')
+            }
+            return res.status(200).json(chore)
+    } catch (err) {
+        return res.status(500).send(err.message)
+    }
+}
+
+const deleteChore = async (req, res) => {
+    try {
+        const { id } = req.params
+        const deleted = await Chore.findByIdAndDelete(id)
+        if (deleted) {
+            return res.status(200).send('Chore deleted')
+        }
+        throw new Error('User not found!')
+    }
+    catch (err) {
         return res.status(500).send(err.message)
     }
 }
@@ -89,7 +129,7 @@ const createReward = async (req, res) => {
     }
 }
 
-const findAllRewards = async (req, res) => {
+const getAllRewards = async (req, res) => {
     try {
         const rewards = await Reward.find()
         return res.status(200).json({ rewards })
@@ -97,6 +137,47 @@ const findAllRewards = async (req, res) => {
         return res.status(500).send(err.message)
     }
 }
+
+const getRewardById = async (req, res) => {
+    try {
+        const { id } = req.params
+        const reward = await Reward.findById(id)
+        if (reward) {
+            return res.status(200).json({ reward })
+        }
+        return res.status(400).send('Reward with the specified id does not exist.')
+    } catch (err) {
+        return res.status(500).send(err.message)
+    }
+} 
+
+const updateReward = async (req, res) => {
+    try {
+        const { id } = req.params
+        const reward = await Reward.findByIdAndUpdate(id, req.body, { new: true})
+            if (!reward) {
+                res.status(500).send('Reward not found')
+            }
+            return res.status(200).json(reward)
+    } catch (err) {
+        return res.status(500).send(err.message)
+    }
+}
+
+const deleteReward = async (req, res) => {
+    try {
+        const { id } = req.params
+        const deleted = await Reward.findByIdAndDelete(id)
+        if (deleted) {
+            return res.status(200).send('Reward deleted')
+        }
+        throw new Error('Reward not found!')
+    }
+    catch (err) {
+        return res.status(500).send(err.message)
+    }
+}
+
 
 
 
@@ -108,7 +189,13 @@ module.exports = {
     updateUser,
     deleteUser,
     createChore,
-    findAllChores,
+    getAllChores,
+    getChoreById,
+    updateChore,
+    deleteChore,
     createReward,
-    findAllRewards
+    getAllRewards,
+    getRewardById,
+    updateReward,
+    deleteReward
 }
--- routes/index.js
+++ routes/index.js
@@ -4,17 +4,37 @@ const router = Router()
 
 router.get('/', (req, res) => res.send('This is landing page!'))
 
+
 router.post('/user', controllers.createUser)
 
 router.get('/user', controllers.getAllUsers)
 
 router.get('/user/:id', controllers.getUserById)
-// 
-// router.get('/roller-coasters/:id', controllers.getRollerCoasterbyId)
-// 
-// router.put('/theme-parks/:id', controllers.updateThemePark)
-// 
-// router.put('/roller-coasters/:id', controllers.updateRollerCoaster)
 
+router.put('/user/:id', controllers.updateUser)
+
+router.delete('/user/:id', controllers.deleteUser)
+
+
+router.post('/chore', controllers.createChore)
+
+router.get('/chore', controllers.getAllChores)
+
+router.get('/chore/:id', controllers.getChoreById)
+
+router.put('/chore/:id', controllers.updateChore)
+
+router.delete('/chore/:id', controllers.deleteChore)
+
+
+router.post('/reward', controllers.createReward)
+
+router.get('/reward', controllers.getAllRewards)
+
+router.get('/reward/:id', controllers.getRewardById)
+
+router.put('/reward/:id', controllers.updateReward)
+
+router.delete('/reward/:id', controllers.deleteReward)
 
 module.exports = router
