6c5d7fb94f0cb5eb75577eba1dfca10443eb6088
David Wu <dwu0508@gmail.com>
2022-04-01 17:16:16 -0500

    db

--- db/index.js
+++ db/index.js
@@ -1,10 +1,28 @@
+// const mongoose = require('mongoose')
+// require('dotenv').config()
+
+// let dbUrl = process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI : 'mongodb://127.0.0.1:27017/choresDatabase'
+
+// mongoose
+//   .connect(dbUrl, { useUnifiedTopology: true, useNewUrlParser: true })
+//   .then(() => {
+//     console.log('Successfully connected to MongoDB!')
+//   })
+//   .catch((e) => {
+//     console.error('Connection error', e.message)
+//   })
+// mongoose.set('debug', true)
+// const db = mongoose.connection
+
+// module.exports = db
+
 const mongoose = require('mongoose')
-require('dotenv').config()
+require('dotenv').config() 
 
 let dbUrl = process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI : 'mongodb://127.0.0.1:27017/choresDatabase'
 
 mongoose
-  .connect(dbUrl, { useUnifiedTopology: true, useNewUrlParser: true })
+  .connect(dbUrl)
   .then(() => {
     console.log('Successfully connected to MongoDB!')
   })
